03c1a546f08e4f432ee50093cb85b1aeed2192e3
jason_chenjh@hotmail.com <jason_chenjh@hotmail.com>
2016-06-14 22:25:32 +0100

    speed up initialization

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ from train import CCA_FILE
 from preprocess.feats import FEATURE_OPTS, data2feats
 from collections import defaultdict
 from CCA import find_answer
+from multiprocessing import Pool, Manager
+from functools import partial
 import argparse
 import pickle as pkl
 import logging
@@ -12,6 +14,16 @@ import numpy as np
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 INF_FREQ = 10
+
+
+def projection(t, P, no_indx):
+    i, v = t
+    v_proj = np.dot(v, P)
+    if no_indx:
+        return v_proj
+    else:
+        return i, v_proj
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Define training process.')
@@ -22,15 +34,9 @@ if __name__ == '__main__':
     feature = args.feature
     INF_FREQ = args.freq
 
+    logging.info("loading vocabulary index")
     with open(VOC_DICT_FILE, 'rb') as f:
         voc_dict = pkl.load(f)
-
-    data = QAs(usage='test', mode='index', voc_dict=voc_dict)
-    feats = None
-    Qs = []
-    As = []
-
-    feats = data2feats(data, feature)
 
     logging.info("loading CCA model")
     # load CCA model
@@ -38,49 +44,53 @@ if __name__ == '__main__':
         U, V = pkl.load(f)
 
     logging.info("constructing testing data")
+    data = QAs(usage='test', mode='index', voc_dict=voc_dict)
+    feats = data2feats(data, feature)
     length = len(feats)
+
     question_indx = 1
     answer_indx = 1
     prev_q = None
     crt_q = None
-    crt_q_proj = None
-    crt_q_indx = None
     q_a_map = defaultdict(list)
+    Qs = []
+    As = []
     for t in feats:
         if answer_indx % INF_FREQ == 0 or answer_indx == length:
             logging.info("loading: %d/%d" % (answer_indx, length))
 
-        data, feat = t
-        crt_q, _ = data
+        (crt_q, crt_a), (crt_q_v, crt_a_v) = t
 
         # question are sorted by alphabet
         # no need to add repeat questions
-        if crt_q == prev_q:
+        if crt_q != prev_q:
             question_indx += 1
-            Qs.append(crt_q_proj)
-        else:
-            # project question use CCA
-            crt_q_proj = np.dot(feat[0], U)
-        prev_q = crt_q
+            Qs.append((question_indx, crt_q_v))
 
-        # project answer use CCA
-        crt_a_proj = np.dot(feat[1], V.T)
         # bind answer with its index
-        As.append((answer_indx, crt_a_proj))
+        As.append((answer_indx, crt_a_v))
         # current answer is one of the correct answer
         q_a_map[question_indx].append(answer_indx)
 
+        prev_q = crt_q
         answer_indx += 1
+
+    logging.info("generating project vector using trained CCA model")
+    with Pool(processes=8) as p:
+        Qs = p.map(partial(projection, P=U, no_indx=False), Qs)
+        As = p.map(partial(projection, P=V.T, no_indx=True), As)
 
     logging.info("testing")
     correct_num = 0
-    for answer_indx, q in enumerate(Qs):
+    indx = 1
+    for question_indx, q in Qs:
         pred = find_answer(q, As)
         # if the found answer is one of the potential answer of the question
-        if pred in q_a_map[q]:
+        if pred in q_a_map[question_indx]:
             # correct
             correct_num += 1
-        logging.warning("tested: %d/%d, get %d correct" % (answer_indx + 1, length, correct_num))
+        logging.warning("tested: %d/%d, get %d correct" % (indx, length, correct_num))
+        indx += 1
 
     # output result
     accuracy = float(correct_num) / len(QAs)
