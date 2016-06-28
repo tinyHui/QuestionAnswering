86d09cf3544832b820f860694aa621b9d0cce10d
jason_chenjh@hotmail.com <jason_chenjh@hotmail.com>
2016-06-28 20:16:01 +0100

    fix a warning

--- train.py
+++ train.py
@@ -1,8 +1,8 @@
 from word2index import VOC_DICT_FILE
 from preprocess.data import ReVerbPairs
-from preprocess.feats import FEATURE_OPTS, data2feats, BoW, WordEmbedding, EMBEDDING_SIZE
+from preprocess.feats import FEATURE_OPTS, data2feats, BoW, WordEmbedding
+from word2embedding import EMBEDDING_SIZE
 from scipy.sparse import csr_matrix
-import numpy as np
 from CCA import train
 import argparse
 import pickle as pkl
@@ -40,12 +40,12 @@ if __name__ == "__main__":
         Q_token_num = len(voc_dict[0].keys())
         A_token_num = len(voc_dict[1].keys())
         # R^ pair num x token num
-        Qs = csr_matrix((pair_num, Q_token_num), dtype=np.float64)
-        As = csr_matrix((pair_num, A_token_num), dtype=np.float64)
+        Qs = csr_matrix((pair_num, Q_token_num), dtype='float64')
+        As = csr_matrix((pair_num, Q_token_num), dtype='float64')
     elif isinstance(feats, WordEmbedding):
         # R^ pair num x embedding size
-        Qs = csr_matrix(pair_num, EMBEDDING_SIZE)
-        As = csr_matrix(pair_num, EMBEDDING_SIZE)
+        Qs = csr_matrix((pair_num, EMBEDDING_SIZE), dtype='float64')
+        As = csr_matrix((pair_num, EMBEDDING_SIZE), dtype='float64')
 
     logging.info("constructing train data")
     length = len(feats)
@@ -55,8 +55,8 @@ if __name__ == "__main__":
             logging.info("loading: %d/%d" % (i, length))
 
         _, feat = t
-        Qs[i-1] = feat[0]
-        As[i-1] = feat[1]
+        Qs[i-1][:] = feat[0]
+        As[i-1][:] = feat[1]
         i += 1
 
     del data, feats, voc_dict
