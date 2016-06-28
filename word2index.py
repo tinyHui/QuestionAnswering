1d63ac5bf56e5800b9c213f32dd52fe1a75aea08
jason_chenjh@hotmail.com <jason_chenjh@hotmail.com>
2016-06-28 23:52:08 +0100

    use old algorithm

--- word2index.py
+++ word2index.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import defaultdict, UserList, UserDict
 from preprocess.data import ReVerbPairs, UNKNOWN_TOKEN, UNKNOWN_TOKEN_INDX
 import logging
 import pickle as pkl
@@ -7,7 +7,18 @@ import sys
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 VOC_DICT_FILE = './bin/word_indx_hash.pkl'
-LOWEST_FREQ = 3
+LOWEST_FREQ = 4
+
+
+def generate_dictionary(token_list):
+    # generate dictionary
+    token_list.insert(UNKNOWN_TOKEN_INDX, UNKNOWN_TOKEN)
+    unique_token_list = set(token_list)
+    word_indx_hash = UserDict(zip(unique_token_list, range(len(unique_token_list))))
+    logging.info("Found %d tokens" % len(unique_token_list))
+    del unique_token_list
+
+    return word_indx_hash
 
 
 if __name__ == "__main__":
@@ -21,26 +32,19 @@ if __name__ == "__main__":
 
     logging.info("Extracting tokens")
     logging.warning("Ignore tokens appears less than %d" % LOWEST_FREQ)
-    token_count_group = {}
-    token_group = defaultdict(list)
-    for line in src_data:
-        for i in src_data.sent_indx:
-            for w in line[i]:
-                try:
-                    if token_count_group[i][w] > LOWEST_FREQ:
-                        token_group[i].append(w)
-                    token_count_group[i][w] += 1
-                except KeyError:
-                    token_count_group[i] = defaultdict(int)
-                    token_count_group[i][w] += 1
-
-    logging.info("Generating token dictionary")
     word_indx_hash_group = {}
     for i in src_data.sent_indx:
-        token_list = token_group[i]
-        word_indx_hash_group[i] = zip(token_list, range(1, len(token_list) + 1))
-        word_indx_hash_group[UNKNOWN_TOKEN] = UNKNOWN_TOKEN_INDX
+        token_list = UserList()
+        token_count = defaultdict(int)
+        for line in src_data:
+            for token in line[i]:
+                if token_count[token] >= LOWEST_FREQ:
+                    token_list.append(token)
+                else:
+                    token_count += 1
+        word_indx_hash_group[i] = generate_dictionary(token_list)
         logging.info("Found %d tokens" % len(token_list))
+        del token_list[:]
 
     logging.info("Saving word index hashing table")
     with open(VOC_DICT_FILE, 'wb') as f:
