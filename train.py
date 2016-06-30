d3368e451a4b013b5c8818d57cc95c7c39aacaed
jason_chenjh@hotmail.com <jason_chenjh@hotmail.com>
2016-06-30 10:34:08 +0100

    use parallel to load data

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from word2index import VOC_DICT_FILE
 from preprocess.data import ReVerbPairs
 from preprocess.feats import FEATURE_OPTS, data2feats
 from scipy.sparse import csr_matrix, vstack
-from collections import UserList
+from multiprocessing import Queue, Process
 from CCA import train
 import argparse
 import pickle as pkl
@@ -11,6 +11,30 @@ import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 CCA_FILE = "./bin/CCA_model_%s.pkl"
 INF_FREQ = 1000  # information message frequency
+PROCESS_NUM = 20
+
+
+def generate(feature_set, q):
+    i = 1
+    Qs = None
+    As = None
+    for feature in feature_set:
+        _, feat = feature
+
+        if isinstance(Qs, type(None)):
+            Qs = csr_matrix(feat[0], dtype='float64')
+            As = csr_matrix(feat[1], dtype='float64')
+        else:
+            Qs = vstack((Qs, feat[0]))
+            As = vstack((As, feat[1]))
+
+        if i % INF_FREQ == 0:
+            q.put((Qs, As))
+            # reset Qs and As
+            Qs = None
+            As = None
+        i += 1
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Define training process.')
@@ -31,34 +55,60 @@ if __name__ == "__main__":
     with open(VOC_DICT_FILE, 'rb') as f:
         voc_dict = pkl.load(f)
 
-    data = ReVerbPairs(usage='train', mode='index', voc_dict=voc_dict)
+    logging.info("constructing train data")
+    data_list = [ReVerbPairs(usage='train', part=i, mode='index', voc_dict=voc_dict) for i in range(PROCESS_NUM)]
+    feats_list = [data2feats(data, feature) for data in data_list]
+    pair_num = sum([len(data) for data in data_list])
+    length = sum([len(feats) for feats in feats_list])
 
-    feats = data2feats(data, feature)
-    pair_num = len(data)
     Qs = None
     As = None
-    Qs_temp = UserList([None] * INF_FREQ)
-    As_temp = UserList([None] * INF_FREQ)
+    pool = []
+    temp_qa = Queue()
+    for i in range(PROCESS_NUM):
+        p = Process(target=generate, args=(feats_list[i], temp_qa))
+        p.start()
 
-    logging.info("constructing train data")
-    length = len(feats)
-    i = 1
-    for t in feats:
-        _, feat = t
+    count = 0
+    while True:
+        # pending until have result
+        temp = temp_qa.get()
+        if temp is None:
+            break
+        Qs_temp, As_temp = temp
+        if isinstance(Qs, type(None)):
+            Qs = csr_matrix(Qs_temp, dtype='float64')
+            As = csr_matrix(As_temp, dtype='float64')
+        else:
+            Qs = vstack((Qs, Qs_temp))
+            As = vstack((As, As_temp))
 
-        if i % INF_FREQ == 0 or i == length:
-            logging.info("loading: %d/%d" % (i, length))
-            if Qs is None:
-                Qs = csr_matrix(Qs_temp, dtype='float64')
-                As = csr_matrix(Qs_temp, dtype='float64')
-            else:
-                Qs = vstack((Qs, Qs_temp))
-                As = vstack((As, As_temp))
+        count += INF_FREQ
+        logging.info("loading: %d/%d" % (count, length))
 
-        Qs_temp[(i-1) % INF_FREQ] = feat[0]
-        As_temp[(i-1) % INF_FREQ] = feat[1]
+    for i in range(PROCESS_NUM):
+        p = pool[i]
+        p.join()
 
-        i += 1
+    # single thread
+    # i = 1
+    # for t in feats:
+    #     _, feat = t
+    #
+    #     if i % INF_FREQ == 0 or i == length:
+    #         logging.info("loading: %d/%d" % (i, length))
+    #         if isinstance(Qs, type(None)):
+    #             Qs = csr_matrix(Qs_temp, dtype='float64')
+    #             As = csr_matrix(As_temp, dtype='float64')
+    #         else:
+    #             Qs = vstack((Qs, Qs_temp))
+    #             As = vstack((As, As_temp))
+    #         del Qs_temp[:], As_temp[:]
+    #
+    #     Qs_temp.append(feat[0])
+    #     As_temp.append(feat[1])
+    #
+    #     i += 1
 
     if not full_svd:
         logging.info("running CCA, using SVDs, k=%d" % k)
