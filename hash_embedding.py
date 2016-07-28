0410cbc4ec51bdad9801cbb9c8f8de77a50c0842
jason_chenjh@hotmail.com <jason_chenjh@hotmail.com>
2016-07-28 22:04:25 +0100

    set occurency == 3 as the UNKNOWN token

--- hash_embedding.py
+++ hash_embedding.py
@@ -27,11 +27,13 @@ if __name__ == "__main__":
         sys.stdout.flush()
         line_num += 1
         word_emb_hash[w] = emb
+    sys.stdout.write("\n")
 
     #
     #  calculate embedding for UNKNOWN token
     #
     # get token occur time
+    logging.info("calculating embedding for UNKNOWN token")
     token_occur_count = defaultdict(int)
     src_data = ReVerbPairs(usage='train', mode='raw_token', grams=1)
     for line in src_data:
@@ -42,16 +44,17 @@ if __name__ == "__main__":
     unknown_emb = np.zeros(EMBEDDING_SIZE, dtype='float32')
     unknown_count = 0
     for token, occur_count in token_occur_count.items():
-        if occur_count == 1:
+        if occur_count == 3:
             try:
+                # for token only occur once and token have trained embedding
                 unknown_emb += word_emb_hash[token]
                 unknown_count += 1
             except KeyError:
                 continue
+    # get average of embedding
     unknown_emb /= float(unknown_count)
     word_emb_hash[UNKNOWN_TOKEN] = unknown_emb
 
-    sys.stdout.write("\n")
     logging.info("Saving word embedding dictionary")
     with open(WORD_EMBEDDING_BIN_FILE, 'wb') as f:
         pkl.dump(word_emb_hash, f, protocol=4)
