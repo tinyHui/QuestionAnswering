9ead0a6918c0f51bd6ea00c1b4d85415174cc373
jason_chenjh@hotmail.com <jason_chenjh@hotmail.com>
2016-08-14 13:36:11 +0100

    split embedding version too slow, split text file instead; split sample and train-test processes into two scripts.

--- best_result.sh
+++ best_result.sh
@@ -2,24 +2,6 @@
 set -u
 set -e
 
-echo "Enter 'total sample lines number', 'sample parts', 'file name', 'file line number'."
-read total_sample_lines_num parts_num
-
-filename='./data/reverb-train.full.emb'
-file_lines_num=43133211
-
-part_lines_num=$(echo "$total_sample_lines_num/$parts_num" | bc)
-max_parts=$(echo "$file_lines_num/$part_lines_num" | bc)
-
-count=1
-for i in $(shuf -i 1-$max_parts -n $parts_num | sort -n); do
-    echo "generating part$count ./data/reverb-train.part$count.emb"
-    start=$(echo "($i-1)*$part_lines_num+1" | bc);
-    end=$(echo "($i*$part_lines_num)" | bc);
-    sed -n "${start},${end}p" $filename > "./data/reverb-train.part$count.emb";
-    count=$(echo "$count+1" | bc);
-done;
-
 for i in $(seq $parts_num); do
     echo "training part$i ./data/reverb-train.part$i.txt"
 
---
+++ sample_reverb_train.py
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -u
+set -e
+
+echo "Enter 'total sample lines number', 'sample parts', 'file name', 'file line number'."
+read total_sample_lines_num parts_num
+
+filename='./data/reverb-train.full.txt'
+file_lines_num=43133211
+
+part_lines_num=$(echo "$total_sample_lines_num/$parts_num" | bc)
+max_parts=$(echo "$file_lines_num/$part_lines_num" | bc)
+
+count=1
+for i in $(shuf -i 1-$max_parts -n $parts_num | sort -n); do
+    echo "generating part$count ./data/reverb-train.part$count.txt"
+    start=$(echo "($i-1)*$part_lines_num+1" | bc);
+    end=$(echo "($i*$part_lines_num)" | bc);
+    sed -n "${start},${end}p" $filename > "./data/reverb-train.part$count.txt";
+
+    count=$(echo "$count+1" | bc);
+done;
