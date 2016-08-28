4fb84f9dd533e93559ef50c87bb195e7e6cb65d9
jason_chenjh@hotmail.com <jason_chenjh@hotmail.com>
2016-08-28 23:57:02 +0100

    fix a bug

--- word2vec.py
+++ word2vec.py
@@ -4,15 +4,15 @@ LOW_FREQ_TOKEN_FILE = './bin/unigram_low_freq_voc.pkl'
 EMBEDDING_SIZE = 300
 
 
-class PairSpliter(object):
-    def __init__(self, data):
-        self.data = data
+# class PairSpliter(object):
+#     def __init__(self, data):
+#         self.data = data
 
-    def __iter__(self):
-        for line in self.data:
-            for i in self.data.sent_indx:
-                # yield [token if token not in self.unknown_tokens else UNKNOWN_TOKEN for token in line[i]]
-                yield line[i]
+#     def __iter__(self):
+#         for line in self.data:
+#             for i in self.data.sent_indx:
+#                 # yield [token if token not in self.unknown_tokens else UNKNOWN_TOKEN for token in line[i]]
+#                 yield line[i]
 
 
 if __name__ == "__main__":
@@ -35,7 +35,8 @@ if __name__ == "__main__":
         def __iter__(self):
             data = ParaphraseWikiAnswer(mode='raw_token')
             for line in data:
-                yield line
+                for i in data.sent_indx:
+                    yield line[i]
 
             data = GigawordRaw()
             for line in data:
@@ -70,7 +71,7 @@ if __name__ == "__main__":
     # with open(LOW_FREQ_TOKEN_FILE, 'wb') as f:
     #     pkl.dump(unknown_tokens, f, protocol=4)
 
-    sentences = PairSpliter(Combine())
+    sentences = Combine()
     # calculate embedding vector
     logging.info("Generating embedding vectors")
     model = Word2Vec(sentences, size=EMBEDDING_SIZE, window=5, min_count=1, workers=40)
